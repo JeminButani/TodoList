5691c006dc02649f598cb23a13c93cf5ed5af27e
JeminButani <95534964+JeminButani@users.noreply.github.com>
2022-01-22 18:49:53 +0530

    my todolist react

---
+++ src/components/todoreact/Todo.js
@@ -0,0 +1,131 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+
+const getLocalData = () => {
+  const list = localStorage.getItem("mytodolist");
+  if (list) {
+    return JSON.parse(list);
+  } else {
+    return [];
+  }
+};
+
+const Todo = () => {
+  const [inputData, setInputData] = useState("");
+  const [items, setItems] = useState(getLocalData());
+  const [isEditItem, setIsEditItem] = useState("");
+  const [toggleButton, setToggleButton] = useState(false);
+
+  // add the items function
+  const addItem = () => {
+    if (!inputData) {
+      alert("Please Fill the Data");
+    } else if (inputData && toggleButton) {
+      setItems(
+        items.map((curElem) => {
+          if (curElem.id === isEditItem) {
+            return { ...curElem, name: inputData };
+          }
+          return curElem;
+        })
+      );
+      setInputData("");
+      setIsEditItem(null);
+      setToggleButton(false);
+    } else {
+      const newInputData = {
+        id: new Date().getTime().toString(),
+        name: inputData,
+      };
+      setItems([...items, newInputData]);
+      setInputData("");
+    }
+  };
+
+  // edit the items
+  const editItem = (index) => {
+    const item_todo_edited = items.find((curElem) => {
+      return curElem.id === index;
+    });
+    setInputData(item_todo_edited);
+    setIsEditItem(index);
+    setToggleButton(true);
+  };
+
+  // delete items
+  const deleteItem = (index) => {
+    const updatedItems = items.filter((curElem) => {
+      return curElem.id !== index;
+    });
+    setItems(updatedItems);
+  };
+
+  // remove all the elements
+  const removeAll = () => {
+    setItems([]);
+  };
+
+  // adding in local storage
+  useEffect(() => {
+    localStorage.setItem("mytodolist", JSON.stringify(items));
+  }, [items]);
+
+  return (
+    <>
+      <div className="main-div">
+        <div className="child-div">
+          <figure>
+            <img src="./images/todo.svg" alt="todologo" />
+            <figcaption>Add your list here ✌️</figcaption>
+          </figure>
+          <div className="addItems">
+            <input
+              type="text"
+              placeholder="✍️ Add Item"
+              className="form-control"
+              value={inputData}
+              onChange={(event) => setInputData(event.target.value)}
+            />
+            {toggleButton ? (
+              <i className="far fa-edit add-btn" onClick={addItem}></i>
+            ) : (
+              <i className="fa fa-plus add-btn" onClick={addItem}></i>
+            )}
+          </div>
+          {/* show our items */}
+          <div className="showItems">
+            {items.map((curElem) => {
+              return (
+                <div className="eachItem" key={curElem.id}>
+                  <h3>{curElem.name}</h3>
+                  <div className="todo-btn">
+                    <i
+                      className="far fa-edit add-btn"
+                      onClick={() => editItem(curElem.id)}
+                    ></i>
+                    <i
+                      className="far fa-trash-alt add-btn"
+                      onClick={() => deleteItem(curElem.id)}
+                    ></i>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          {/* {remove all items button} */}
+          <div className="showItems">
+            <button
+              className="btn effect04"
+              data-sm-link-text="Remove All"
+              onClick={removeAll}
+            >
+              <span>Check List</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
